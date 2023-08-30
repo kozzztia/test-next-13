1a2257d83a94c98c5da51b14ea8d6441ae019866
kozzztia <kozzztia@gmail.com>
2023-08-30 11:01:25 +0300

    fix 425px

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import AboutLayout from "@/components/layout/AboutLayout/AboutLayout";
+import {dictionary} from "@/public/dictionary/dictionary";
+import style from "@/public/styles/about.module.css";
+import HeaderTitle from "@/ui-kit/title/HeaderTitle";
+import Link from "next/link";
 
 
-const Layout = ({children} : {children : React.ReactNode}) => {
+const AboutLayout = ({children} : {children : React.ReactNode}) => {
+    const {title} = dictionary.en.about;
     return (
-        <AboutLayout>
-            {children}
-        </AboutLayout>
+        <div className={style.layout}>
+            <HeaderTitle title={title} className={style.title}/>
+            <ul className={style.list}>
+                <Link href={"/about"}>back</Link>
+                <Link href={"/about/contacts"}>contacts</Link>
+                <Link href={"/about/team"}>team</Link>
+            </ul>
+            <div className={style.content}>
+                {children}
+            </div>
+        </div>
     );
 };
 
-export default Layout;
+export default AboutLayout;
--- components/viewer/Posts.tsx
+++ components/viewer/Posts.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 import style from "@/public/styles/posts.module.css";
 import Link from "next/link";
 import {dictionary} from "@/public/dictionary/dictionary"
-import HeaderTitle from "@/ui-kit/title/HeaderTitle";
 
-const Posts = () => {
-    const { createNewPost, text } = dictionary.en.posts
+type dataType = {
+    userId : number;
+    id :number;
+    title : string;
+    body : string;
+}
+
+const gatData = async ():Promise<dataType[]> => {
+    const data  = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return data.json()
+}
+const Posts = async () => {
+    const posts : dataType[] = await gatData()
+    const { createNewPost, text } = dictionary.en.posts;
     return (
         <div className={style.posts}>
             <span className={style.posts__text}>{text}</span>
             <Link href={"posts/create"} className={style.link}>{createNewPost}</Link>
+            <ul>
+                {
+                    posts.map(item  => <li><Link key={item.id} href={`/posts/${item.id}`}>{item.title}</Link></li>)
+                }
+            </ul>
         </div>
     );
 };
